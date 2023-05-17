dab70d89e2abc93539b329b2573971c690391026
Calvin Ho <calvin@arevolabs.com>
2023-05-17 17:09:29 +0700

    Added datacrawling script for crawling heroes profile images from LOL page

---
+++ datacrawling.py
@@ -0,0 +1,16 @@
+from lxml import html
+import requests
+import re
+from urllib import request
+
+res = requests.get('https://leagueoflegends.fandom.com/wiki/League_of_Legends:_Wild_Rift')
+tree = html.fromstring(res.text)
+urls = [re.sub(r"(^(?!png).+png)(.*)", r"\1", i) for i in tree.xpath('//div[@class="columntemplate"]/ul/li/span/span/a/img/@data-src')]
+for url in urls:
+    filename = url.split('/')[-1]
+    print(f"{url} \n")
+    print(f"filename: {filename}")
+    response = request.urlretrieve(url, "D:\\IT\\AI\\Eklipse\\Eklipse_test\\crawled_images\\"+filename)
+
+
+
